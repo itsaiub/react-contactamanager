abb950ef5db7c1eeb61bf1d44e532f493b19f2ec
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-10-18 09:31:16 +0600

    adding context reducer for actions

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -2,13 +2,7 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 import { Consumer } from "../context";
 class Contacts extends Component {
-  deleteContact = id => {
-    const { contacts } = this.state;
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({ contacts: newContacts });
-  };
-
-  render() {
+   render() {
     return (
       <Consumer>
         {value => {
@@ -19,7 +13,6 @@ class Contacts extends Component {
                 <Contact
                   key={contact.id}
                   contact={contact}
-                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
                 />
               ))}
             </React.Fragment>
--- src/context.js
+++ src/context.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'DELETE_CONTACT':
+    return {
+      ...state,
+      contacts: state.contacts.filter(contact => contact.id !== action.payload)
+    }
+
+    default: 
+    return state;
+  }
+}
 export class Provider extends Component {
   state = {
     contacts: [
@@ -23,7 +35,10 @@ export class Provider extends Component {
         email: "doe@gmail.com",
         phone: "444-444-5555"
       }
-    ]
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action))
+    }
   };
 
   render() {
