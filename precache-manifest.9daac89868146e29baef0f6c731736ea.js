433271bd98b407dc6d3a7fcf7f9e5ae1455acbe7
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-10-18 23:51:31 +0600

    Updates

---
+++ precache-manifest.9daac89868146e29baef0f6c731736ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e40d23b8b61876c686f",
+    "url": "/react-contactmanager/static/js/runtime~main.3e40d23b.js"
+  },
+  {
+    "revision": "4d3e14c8657eaad0c965",
+    "url": "/react-contactmanager/static/js/main.4d3e14c8.chunk.js"
+  },
+  {
+    "revision": "c43e5b9ba09230feb909",
+    "url": "/react-contactmanager/static/js/1.c43e5b9b.chunk.js"
+  },
+  {
+    "revision": "4d3e14c8657eaad0c965",
+    "url": "/react-contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "c43e5b9ba09230feb909",
+    "url": "/react-contactmanager/static/css/1.dda2dba7.chunk.css"
+  },
+  {
+    "revision": "19fd4b1d551cecbaafc4165d6a987390",
+    "url": "/react-contactmanager/index.html"
+  }
+];
